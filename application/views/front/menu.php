28321a54705899e66e6d955f01e016681bacf8b2
Luvcid <70992846+Luvcid@users.noreply.github.com>
2023-04-11 14:50:46 +0700

    Initial commit

---
+++ application/views/front/menu.php
@@ -0,0 +1,32 @@
+<li>
+                            <a href="<?php  echo base_url().''?>" class="">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Profil</a>
+                            <ul>
+								<li>
+                                    <a href="<?php echo base_url().'about';?>"><span>&nbsp;&nbsp;&nbsp;- </span>Tentang Kami</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo base_url().'testimoni';?>"><span>&nbsp;&nbsp;&nbsp;- </span>Testimoni</a>
+                                </li>
+                                
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="<?php echo base_url().'wisata_post'?>">Wisata</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo base_url().'paket_tour'?>">Paket Tour</a>
+                        </li>
+                         <li>
+                            <a href="<?php echo base_url().'konfirmasi'?>">Konfirmasi</a>
+                        </li>
+                            
+						<li>
+                        </li>
+                        <li>
+                        <li>
+                            <a href="<?php echo base_url().'kontak'?>">Hubungi Kami</a>
+                        </li>
+                    </ul>
